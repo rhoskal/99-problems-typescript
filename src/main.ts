b067a507da6b8f57514962683e473bcfd1285c4b
Hans Hoffman <github.backrest158@passmail.net>
2024-06-22 11:01:54 -0300

    problem 2

--- src/main.ts
+++ src/main.ts
@@ -15,3 +15,20 @@ export const last = <T>(xs: ReadonlyArray<T>): T | null => {
     return last(xs.slice(1));
   }
 };
+
+/*
+ * Problem 2
+ *
+ * Find the last two (last and penultimate) elements of a list.
+ */
+export const last_two = <T>(xs: ReadonlyArray<T>): ReadonlyArray<T> | null => {
+  if (xs.length === 0 || xs.length === 1) {
+    return null;
+  }
+
+  if (xs.length === 2) {
+    return xs;
+  }
+
+  return last_two(xs.slice(1));
+};
--- test/main.spec.ts
+++ test/main.spec.ts
@@ -1,10 +1,17 @@
 import { expect, test } from "vitest";
 // import * as fc from "fast-check";
 
-import { last } from "../src/main";
+import { last, last_two } from "../src/main";
 
 test("Return the last element in a given list", () => {
   expect(last([])).toEqual(null);
   expect(last(["a"])).toEqual("a");
   expect(last([1, 2, 3])).toEqual(3);
+});
+
+test("Return the last two elements in a given list", () => {
+  expect(last_two([])).toEqual(null);
+  expect(last_two([true])).toEqual(null);
+  expect(last_two(["a", 1])).toEqual(["a", 1]);
+  expect(last_two([1, 2, 3])).toEqual([2, 3]);
 });
