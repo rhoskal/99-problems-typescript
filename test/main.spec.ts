3badbd5f5497781eb88437d4c4cca99f238c6886
Hans Hoffman <github.backrest158@passmail.net>
2024-06-23 15:58:33 -0300

    add leading zeros to count

--- test/main.spec.ts
+++ test/main.spec.ts
@@ -22,39 +22,39 @@ import {
   slice,
 } from "../src/main";
 
-test("[1] Return the last element of a list", () => {
+test("[01] Return the last element of a list", () => {
   expect(last([])).toEqual(null);
   expect(last(["a"])).toEqual("a");
   expect(last([1, 2, 3])).toEqual(3);
 });
 
-test("[2] Return the last two elements of a list", () => {
+test("[02] Return the last two elements of a list", () => {
   expect(last_two([])).toEqual(null);
   expect(last_two([true])).toEqual(null);
   expect(last_two(["a", 1])).toEqual(["a", 1]);
   expect(last_two([1, 2, 3])).toEqual([2, 3]);
 });
 
-test("[3] Return nth element of a list", () => {
+test("[03] Return nth element of a list", () => {
   expect(nth(2)([])).toEqual(null);
   expect(nth(2)([1, 2])).toEqual(2);
   expect(nth(-2)([1, 2])).toEqual(null);
   expect(nth(2)(["a", "b", "c", "d", "e"])).toEqual("b");
 });
 
-test("[4] Return the length of a list", () => {
+test("[04] Return the length of a list", () => {
   expect(length([])).toEqual(0);
   expect(length([1])).toEqual(1);
   expect(length(["a", "b", "c", "d", "e"])).toEqual(5);
 });
 
-test("[5] Return the items of a list reversed", () => {
+test("[05] Return the items of a list reversed", () => {
   expect(reverse([])).toEqual([]);
   expect(reverse([1])).toEqual([1]);
   expect(reverse([1, 2, 3])).toEqual([3, 2, 1]);
 });
 
-test("[6] Return the items of a list reversed", () => {
+test("[06] Return the items of a list reversed", () => {
   expect(is_palindrome([])).toBe(true);
   expect(is_palindrome([1])).toBe(true);
   expect(is_palindrome([1, 2, 3])).toBe(false);
@@ -64,21 +64,21 @@ test("[6] Return the items of a list reversed", () => {
   expect(is_palindrome([1, 2, 4, 8, 16, 8, 4, 2, 1])).toBe(true);
 });
 
-test("[7] Return a flattened list", () => {
+test("[07] Return a flattened list", () => {
   expect(flatten([1, [2, 3]])).toStrictEqual([1, 2, 3]);
   expect(flatten([[2, 3], 1])).toStrictEqual([2, 3, 1]);
   expect(flatten(["a", ["b", ["c", "d"], "e"]])).toStrictEqual(["a", "b", "c", "d", "e"]);
   expect(flatten([[["a"]]])).toStrictEqual(["a"]);
 });
 
-test("[8] Remove consecutive duplicates", () => {
+test("[08] Remove consecutive duplicates", () => {
   expect(compress(["a", "a", "b", "c", "c"])).toStrictEqual(["a", "b", "c"]);
   expect(
     compress(["a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e"]),
   ).toStrictEqual(["a", "b", "c", "a", "d", "e"]);
 });
 
-test("[9] Pack/combine duplicates", () => {
+test("[09] Pack/combine duplicates", () => {
   expect(pack(["a", "a", "b", "c", "c"])).toStrictEqual(["aa", "b", "cc"]);
   expect(
     pack(["a", "a", "a", "a", "b", "c", "c", "a", "a", "d", "e", "e", "e", "e"]),
