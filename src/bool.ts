78f30e539b9b680bd0102a832862317e8fce2234
Hans Hoffman <github.backrest158@passmail.net>
2024-07-14 21:20:37 +1200

    formatting binary fns file

--- src/bool.ts
+++ src/bool.ts
@@ -1,8 +1,23 @@
-export const and = (a: boolean, b: boolean): boolean => a && b;
-export const not = (a: boolean): boolean => !a;
-export const or = (a: boolean, b: boolean): boolean => a || b;
-export const nand = (a: boolean, b: boolean): boolean => not(a && b);
-export const nor = (a: boolean, b: boolean): boolean => not(a || b);
+export const and = (a: boolean, b: boolean): boolean => {
+  return a && b;
+};
+
+export const not = (a: boolean): boolean => {
+  return !a;
+};
+
+export const or = (a: boolean, b: boolean): boolean => {
+  return a || b;
+};
+
+export const nand = (a: boolean, b: boolean): boolean => {
+  return not(a && b);
+};
+
+export const nor = (a: boolean, b: boolean): boolean => {
+  return not(a || b);
+};
+
 export const xor = (a: boolean, b: boolean): boolean => {
   if (a === true && b === false) {
     return true;
